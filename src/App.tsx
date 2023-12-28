cb5bbcdbdd07b7de4aa4c96e8e054edb1be09bbf
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2023-12-28 18:07:22 +0900

    Design: Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Header } from './components/Header';
 
 const Root = styled.div``;
 function App() {
-  return <Root>SeongHoon's PortFolio Website</Root>;
+  return (
+    <Root>
+      <Header />
+      SeongHoon's PortFolio Website
+    </Root>
+  );
 }
 
 export default App;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,44 @@
-import { FC } from 'react';
 import styled from 'styled-components';
+import { HEADER_HEIGHT } from '../constants/layout';
 
-const Root = styled.div``;
+const Root = styled.header`
+  padding-top: 10px 0 5px 0;
+  position: fixed;
+  top: 0;
+  left: 40%;
+  height: ${HEADER_HEIGHT}px;
+  background-color: rgba(0, 0, 0, 0.6);
+  width: 50%;
+  color: ${props => props.theme.bgColor};
 
-type HeaderProps = {
-  className?: string;
-};
+  border-radius: 2px 2px 30px 30px;
+`;
+const Items = styled.ul`
+  width: 100%;
+  height: 100%;
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  text-align: center;
+`;
+const Item = styled.li`
+  place-self: center;
+  opacity: 0.6;
+  cursor: pointer;
+  &:hover {
+    opacity: 1;
+  }
+`;
 
-export const Header: FC<HeaderProps> = () => {
-  return <Root>Header</Root>;
+export const Header = () => {
+  return (
+    <Root>
+      <Items>
+        <Item>First</Item>
+        <Item>Second</Item>
+        <Item>Third</Item>
+        <Item>Fourth</Item>
+        <Item>Fifth</Item>
+      </Items>
+    </Root>
+  );
 };
