da6ad2431406110e96ea5f6c57130a9e362f9c2c
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2023-12-30 20:21:47 +0900

    Feat: AboutCard Animation - 없어질 때 수정필요

--- src/components/AboutCard.tsx
+++ src/components/AboutCard.tsx
@@ -1,12 +1,13 @@
 import { IAboutCard } from 'constants/aboutCardList';
+import { Variants, motion } from 'framer-motion';
+import { useScrollAnimation } from 'hooks/useScrollAnimation';
 import styled from 'styled-components';
 
-const Root = styled.div`
+const Root = styled(motion.div)`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
-  /* background-color: aliceblue; */
   flex-basis: 135px;
   flex-grow: 1;
   min-height: 200px;
@@ -33,10 +34,30 @@ const Description = styled.p`
   font-weight: 300;
   line-height: 20px;
 `;
-
+const RootVar: Variants = {
+  start: {
+    opacity: 0,
+    rotateY: 180,
+  },
+  end: {
+    opacity: 1,
+    rotateY: 360,
+    transition: {
+      type: 'tween',
+      bounce: 0.6,
+      duration: 1,
+    },
+  },
+};
 export const AboutCard = ({ title, description, imgUrl }: IAboutCard) => {
+  const { ref, isVisible } = useScrollAnimation(0.2);
   return (
-    <Root>
+    <Root
+      variants={RootVar}
+      ref={ref}
+      initial="start"
+      animate={isVisible ? 'end' : 'start'}
+    >
       <CardImg src={imgUrl} />
       <TypoContainer>
         <Title>{title}</Title>
